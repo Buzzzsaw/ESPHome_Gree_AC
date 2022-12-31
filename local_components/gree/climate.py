6bdf92a96686628ffd6bc2058786d00c9dbc8016
Alexis Leclerc <alexis.leclerc.1337@gmail.com>
2022-12-31 13:21:34 -0500

    Python layer

---
+++ local_components/gree/climate.py
@@ -0,0 +1,26 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.components import climate
+from esphome.const import (
+  CONF_ID, 
+  CONF_IR_PIN
+)
+
+CODEOWNERS = ["@buzzzsaw"]
+
+gree_ns = cg.esphome_ns.namespace("gree")
+GreeClimate = gree_ns.class_("GreeClimate", climate.Climate)
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(GreeClimate),
+        cv.Optional(CONF_IR_PIN): pins.gpio_output_pin_schema,
+    }
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
