4988ce0e2371fd3848f0c079b1688683118328ba
Alexis Leclerc <alexis.leclerc.1337@gmail.com>
2024-03-21 21:30:12 -0400

    Forgot include

--- components/gree/gree.h
+++ components/gree/gree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esphome/components/climate/climate.h"
+#include "esphome/components/select/select.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/core/gpio.h"
